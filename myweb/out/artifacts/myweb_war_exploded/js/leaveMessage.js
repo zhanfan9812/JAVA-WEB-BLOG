30c4ec898ab84b72cfb2b472f3df3f12d4bce1d4
zhanfan9812 <zhanfan9812@users.noreply.github.com>
2019-07-03 11:04:21 +0800

    Revert "2019-6-19"
    This reverts commit f03e3a514bed1a3786ff6d87f773b07184ad25d9.

--- myweb/out/artifacts/myweb_war_exploded/js/leaveMessage.js
+++
@@ -1,9 +0,0 @@
-function checkTitle() {
-    if(document.getElementById("title").value.length==""){
-        document.getElementById("tips_title").innerHTML="<em style='color: red'>标题不能为空</em>"
-        return false;
-    }else{
-        document.getElementById("tips_title").innerHTML="<em style='color: green'>OK</em>"
-        return true;
-    }
-}
